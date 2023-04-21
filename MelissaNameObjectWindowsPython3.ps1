4d076e291f0abe1a59e44399de9979e25db797a5
Melissa <>
2023-04-21 08:24:33 -0700

    Tested successfully using Melissa data files for 2023.04

--- MelissaNameObjectWindowsPython3.ps1
+++ MelissaNameObjectWindowsPython3.ps1
@@ -19,7 +19,7 @@ class DLLConfig {
 
 ######################### Config ###########################
 
-$RELEASE_VERSION = '2023.03'
+$RELEASE_VERSION = '2023.04'
 $ProductName = "DQ_NAME_DATA"
 
 # Uses the location of the .ps1 file 
--- README.md
+++ README.md
@@ -27,7 +27,7 @@ And return
 
 - Windows 64-bit Python 3.8.7
 - Powershell 5.1
-- Melissa data files for 2023-03
+- Melissa data files for 2023-04
 
 ## Required File(s) and Programs
 
