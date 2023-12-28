97ea428a46b2ddb0750fa07bda23457a6bc2e394
Melissa <>
2023-12-27 16:18:00 -0800

    Tested successfully using Melissa data files for 2023.12

--- MelissaNameObjectWindowsPython3.ps1
+++ MelissaNameObjectWindowsPython3.ps1
@@ -19,7 +19,7 @@ class FileConfig {
 
 ######################### Config ###########################
 
-$RELEASE_VERSION = '2023.11'
+$RELEASE_VERSION = '2023.12'
 $ProductName = "DQ_NAME_DATA"
 
 # Uses the location of the .ps1 file 
