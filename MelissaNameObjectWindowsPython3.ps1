2a1df01d9342515e6fd9c504e69a645a422f6c18
Melissa <>
2026-08-11 23:08:56 -0700

    Tested successfully using Melissa data files for 2026.08

--- MelissaNameObjectWindowsPython3.ps1
+++ MelissaNameObjectWindowsPython3.ps1
@@ -19,7 +19,7 @@ class FileConfig {
 
 ######################### Config ###########################
 
-$RELEASE_VERSION = '2026.07'
+$RELEASE_VERSION = '2026.08'
 $ProductName = "DQ_NAME_DATA"
 
 # Uses the location of the .ps1 file 
--- README.md
+++ README.md
@@ -26,7 +26,7 @@ And return
 
 ## Tested Environments
 - Windows 11 64-bit Python 3.8.7, Powershell 5.1
-- Melissa data files for 2026-07
+- Melissa data files for 2026-08
 
 ## Required File(s) and Programs
 
